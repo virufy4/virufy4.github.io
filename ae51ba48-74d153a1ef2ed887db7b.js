c771cee6957368d7766b79433cb299edfabe4ee3
agustinsant <agustinsant@bitbucket.org>
2022-05-27 17:49:27 -0300

    Updates

---
+++ ae51ba48-74d153a1ef2ed887db7b.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[260],{3969:function(t,n,a){a.d(n,{UXH:function(){return u}});var c=a(5566);function u(t){return(0,c.w_)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M256 32C167.67 32 96 96.51 96 176c0 128 160 304 160 304s160-176 160-304c0-79.49-71.67-144-160-144zm0 224a64 64 0 1164-64 64.07 64.07 0 01-64 64z"}}]})(t)}}}]);
+//# sourceMappingURL=ae51ba48-74d153a1ef2ed887db7b.js.map
