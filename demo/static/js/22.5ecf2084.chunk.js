4cfb5cbaddd44b83622c6780929cd10980a9bbbf
Jonatan Jaskilioff <jonatanjas@gmail.com>
2022-08-02 10:25:43 -0300

    Updates

---
+++ demo/static/js/22.5ecf2084.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonppwa-demo"]=this["webpackJsonppwa-demo"]||[]).push([[22],{283:function(t,e,a){"use strict";a.r(e);var n=a(282);a.d(e,"default",(function(){return n.default}))}}]);
+//# sourceMappingURL=22.5ecf2084.chunk.js.map
