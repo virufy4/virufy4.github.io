315e91a24f15be7f8716b8daf661c251cc48bd22
Ignacio Liotti <ignacio.liotti@devlights.com>
2022-08-11 09:10:44 -0300

    create new press release landing page for barda content

---
+++ src/pages/barda-press-release.jsx
@@ -0,0 +1,176 @@
+import React from "react";
+import bgWave from "../images/logos/background-wave.png";
+import Layout from "../components/Layout";
+import { Link } from "gatsby-plugin-intl";
+import SEO from "../components/SEO";
+
+export default function Paper() {
+  return (
+    <Layout>
+      <SEO title="Virufy | Barda" />
+      <div className={`px-6 py-8`}>
+        <img
+          className="-z-10 absolute w-screen left-0"
+          src={bgWave}
+          alt="waves"
+        />
+        <h1>INTERNAL AND EXTERNAL PRESS RELEASE</h1>
+        <h1>THE COVID-19 DETECTION FOUNDATION (dba VIRUFY)</h1>
+        <p className="mt-4 mb-8">July 22, 2022</p>
+        <p className="font-bold mt-4">
+          THE COVID-19 DETECTION FOUNDATION (dba VIRUFY) AND BARDA ANNOUNCE
+          CONTRACT TO RESEARCH & DEVELOP COVID-19 SCREENING THROUGH ARTIFICIAL
+          INTELLIGENCE / MACHINE LEARNING TECHNOLOGY
+        </p>
+        <p className="font-bold mt-4">
+          THE COVID-19 DETECTION FOUNDATION (dba VIRUFY) COVID-19 SCREENING
+          THROUGH ARTIFICIAL INTELLIGENCE / MACHINE LEARNING TECHNOLOGY IS THE
+          NEWEST MEMBER OF BARDA’S DIVISION OF RESEARCH, INNOVATION, AND
+          VENTURES (DRIVe) MEDICAL COUNTERMEASURES PORTFOLIO
+        </p>
+        <p className="mt-4">
+          SILICON VALLEY, CA. (July 22, 2022) – The COVID-19 Detection
+          Foundation (dba Virufy) a 501(c)(3) non-profit research organization ({" "}
+          <Link href="https://virufy.org/" className="text-blue">
+            https://virufy.org/
+          </Link>
+          ) developing a smartphone app for rapid COVID-19 screening through
+          artificial intelligence (AI) / machine learning (ML) technology, today
+          announced that it has been awarded a contract from the Biomedical
+          Advanced Research and Development Authority (BARDA), part of the
+          Office of the Assistant Secretary for Preparedness and Response
+          (ASPR), within the U.S. Department of Health and Human Services (HHS),
+          a U.S. government agency.
+        </p>
+        <p className="mt-4">
+          This project aims to research and develop COVID-19 screening through
+          AI / ML Technology. Virufy’s patented and patent-pending solution
+          gathers a patient’s cough / speech data and then, using AI / ML
+          algorithm, rapidly analyzes that data against known COVID-19
+          Polymerase chain reaction (PCR)-positive, and Covid-19 PCR-negative
+          cough datasets to indicate potential active COVID-19 infection. PCR
+          testing is universally acknowledged as the gold-standard in molecular
+          diagnostics and COVID-19 testing.
+        </p>
+        <p className="mt-4">
+          This Virufy project aims to empower individuals, clinicians,
+          employers, and governments with early, actionable COVID-19
+          information, to provide cost-effective, faster results for capturing
+          and predicting health events anda reduction in overburdening scarce
+          healthcare resources so that timely responses and medical
+          interventions can be better deployed.
+        </p>
+        <p className="mt-4">
+          While Virufy has obtained a contract with BARDA, the limited funds are
+          contractually restricted to contract deliverables and data collection,
+          and we remain a 501(c)(3) entity principally staffed by volunteers and
+          pro bono vendors.
+        </p>
+        <p className="mt-4">
+          “We are profoundly appreciative of the hundreds of volunteers and
+          dozens of pro-bono firms that have selflessly donated their time,
+          creativity, expertise, and compassion in support of our Mission of
+          public good, and for BARDA’s / DRIVe’s generous support to fund the
+          advancement of Virufy’s Covid-19 screening through AI / ML technology”
+          said Virufy CEO, Amil Khanzada. This project has been funded in part
+          with Federal funds from HHS/ASPR and BARDA, under Contract No.
+          75A50122C00034.
+        </p>
+        <p className="font-bold mt-8">Additional Information - </p>
+        <p className="mt-4">
+          This award is one component of BARDA’s rapidly expanding DRIVe Medical
+          countermeasures portfolio, visit https://drive.hhs.gov to learn more.
+        </p>
+        <p className="font-bold mt-8">
+          About the COVID-19 Detection Foundation (dba Virufy) -
+        </p>
+        <p className="mt-4">
+          The COVID-19 Detection Foundation (dba Virufy) is a 501(c)(3)
+          nonprofit research organization developing artificial intelligence
+          (AI) / machine learning (ML) technology offering superior, actionable
+          COVID-19 information, to provide faster results for capturing and
+          predicting health events so that timely responses and medical
+          interventions can be deployed.
+        </p>
+        <p className="mt-4">
+          About HHS, ASPR, and BARDA: The U.S. Department of Health and Human
+          Services works to enhance and protect the health and well-being of all
+          Americans, providing for effective health and human services and
+          fostering advances in medicine, public health, and social services.
+          Within HHS,
+          <Link
+            href="https://gcc02.safelinks.protection.outlook.com/?url=http%3A%2F%2Faspr.hhs.gov%2F&data=05%7C01%7CElizabeth.Kane%40hhs.gov%7Cb08b8231af95400491fa08da638b2025%7Cd58addea50534a808499ba4d944910df%7C0%7C0%7C637931746429939141%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C3000%7C%7C%7C&sdata=3%2BMUqUprhLMbSMhD94xCfgrL7BKrhWJWnokEcI23Gmw%3D&reserved=0"
+            className="text-blue"
+          >
+            &nbsp;ASPR&nbsp;
+          </Link>
+          leads the nation’s medical and public health preparedness for,
+          response to, and recovery from disasters and public health
+          emergencies. As part of ASPR,
+          <Link
+            href="https://gcc02.safelinks.protection.outlook.com/?url=https%3A%2F%2Fwww.medicalcountermeasures.gov%2F&data=05%7C01%7CElizabeth.Kane%40hhs.gov%7Cb08b8231af95400491fa08da638b2025%7Cd58addea50534a808499ba4d944910df%7C0%7C0%7C637931746429939141%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C3000%7C%7C%7C&sdata=Lr3bXCSJexmefnIfO3OTh%2FDfwaqubVBfDBpEf7lZyXU%3D&reserved=0"
+            className="text-blue"
+          >
+            &nbsp;BARDA&nbsp;
+          </Link>{" "}
+          provides a comprehensive, integrated, portfolio approach to the
+          advanced research and development, innovation, acquisition, and
+          manufacturing of medical countermeasures – vaccines, drugs,
+          therapeutics, diagnostic tools, and non-pharmaceutical products for
+          public health emergency threats.
+        </p>
+        <p className="mt-4">
+          To date, BARDA-supported products have achieved
+          <Link
+            href="https://www.medicalcountermeasures.gov/barda/fdaapprovals/"
+            className="text-blue"
+          >
+            &nbsp;over 60 FDA approvals, licensures or clearances.&nbsp;
+          </Link>
+          To learn more about
+          <Link
+            href="https://www.medicalcountermeasures.gov/app/barda/coronavirus/COVID19.aspx"
+            className="text-blue"
+          >
+            &nbsp;BARDA's COVID-19 Portfolio&nbsp;
+          </Link>
+          and
+          <Link
+            href="https://www.medicalcountermeasures.gov/app/barda/COVIDTimeline.aspx#event-bardaacovid-19aresponseatimeline"
+            className="text-blue"
+          >
+            &nbsp;BARDA’s COVID-19 Response
+          </Link>
+          , visit&nbsp;
+          <Link
+            href="http://www.medicalcountermeasures.gov/"
+            className="text-blue"
+          >
+            www.medicalcountermeasures.gov/.
+          </Link>
+        </p>
+        <p className="font-bold mt-8">Published Works - </p>
+        <p className="mt-4">
+          May be found at
+          <Link href="https://virufy.org/paper" className="text-blue">
+            &nbsp;https://virufy.org/paper&nbsp;
+          </Link>
+        </p>
+        <p className="font-bold mt-8">Contact -</p>
+        <p className="mt-4">
+          For more information about Virufy, please visit
+          <Link href="https://virufy.org/" className="text-blue">
+            &nbsp;https://virufy.org/&nbsp;
+          </Link>
+        </p>
+        <p className="mt-4">
+          For information pertaining to this press release, please contact
+          Patrick O’Neill, Managing Partner, Sherlock Communications -
+          <Link href="virufy@sherlockcomms.com" className="text-blue">
+            &nbsp;virufy@sherlockcomms.com&nbsp;
+          </Link>
+        </p>
+      </div>
+    </Layout>
+  );
+}
