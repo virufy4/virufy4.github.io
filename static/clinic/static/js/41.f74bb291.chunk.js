8dfda7c40418b0da14706e4a14d5e18ff78bafdb
Maximiliano Duthey <maxi.duthey@xoor.io>
2022-11-07 16:54:28 -0300

    Update clinic to v1.1.40

---
+++ static/clinic/static/js/41.f74bb291.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpvirufy-study"]=this["webpackJsonpvirufy-study"]||[]).push([[41],{321:function(t,u,s){"use strict";s.r(u);var i=s(320);s.d(u,"default",(function(){return i.default}))}}]);
+//# sourceMappingURL=41.f74bb291.chunk.js.map
