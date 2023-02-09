ef38fc1d86540d94d181c0f5b036cab24e732c72
Juan Almanza <hi@scidroid.co>
2023-02-09 16:55:15 -0500

    Updates

---
+++ compensar/static/js/43.f7eb3654.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpcompensar-app"]=this["webpackJsonpcompensar-app"]||[]).push([[43],{372:function(e,t,n){"use strict";n.r(t);var a=n(9),c=n.n(a),r=n(464),p=n(285);test("renders learn react link",(function(){var e=(0,Object(r.a)(c.a.createElement(p.default,null)).getByText)(/learn react/i);expect(e).toBeInTheDocument()}))}}]);
+//# sourceMappingURL=43.f7eb3654.chunk.js.map
