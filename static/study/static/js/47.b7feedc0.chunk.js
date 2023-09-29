b730723a73b0a0cdf3a1c3a83830731e76e13966
Maximiliano Duthey <maxi.duthey@xoor.io>
2023-09-29 12:07:18 -0300

    Update study to v1.1.50

---
+++ static/study/static/js/47.b7feedc0.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpvirufy-study"]=this["webpackJsonpvirufy-study"]||[]).push([[47],{334:function(t,u,s){"use strict";s.r(u);var i=s(333);s.d(u,"default",(function(){return i.default}))}}]);
+//# sourceMappingURL=47.b7feedc0.chunk.js.map
---
+++ static/study/static/js/50.b1ecebf7.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpvirufy-study"]=this["webpackJsonpvirufy-study"]||[]).push([[50],{346:function(t,u,s){"use strict";s.r(u);var i=s(345);s.d(u,"default",(function(){return i.default}))}}]);
+//# sourceMappingURL=50.b1ecebf7.chunk.js.map
