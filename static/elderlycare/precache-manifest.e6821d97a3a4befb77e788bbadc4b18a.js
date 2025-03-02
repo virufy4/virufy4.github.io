894d394bedec8f9287424c975b6c7468b6fdb123
J-Gaynor <josephgaynor12@gmail.com>
2025-03-02 13:03:22 +0900

    deploy fix attempt

---
+++ static/elderlycare/precache-manifest.e6821d97a3a4befb77e788bbadc4b18a.js
@@ -0,0 +1,406 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f7953a36340775dd5603625f0398b4c",
+    "url": "/elderlycare/index.html"
+  },
+  {
+    "revision": "acb7f4a9ed9965622c98",
+    "url": "/elderlycare/static/css/60.8d4cdce2.chunk.css"
+  },
+  {
+    "revision": "d5008c2c52f58082e4aa",
+    "url": "/elderlycare/static/js/0.1d2ff7b9.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/elderlycare/static/js/0.1d2ff7b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95f9cd9d86915f6b747d",
+    "url": "/elderlycare/static/js/1.71108845.chunk.js"
+  },
+  {
+    "revision": "0456977c883db6ec847e",
+    "url": "/elderlycare/static/js/10.a8ef4d54.chunk.js"
+  },
+  {
+    "revision": "281e7a8408a94c1807dd",
+    "url": "/elderlycare/static/js/11.abdffb40.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/elderlycare/static/js/11.abdffb40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6b1ce133bd7172516cb",
+    "url": "/elderlycare/static/js/12.748482a3.chunk.js"
+  },
+  {
+    "revision": "f84665722f248f08f592",
+    "url": "/elderlycare/static/js/13.26d58fa4.chunk.js"
+  },
+  {
+    "revision": "b077108d6a2dbfa0937e",
+    "url": "/elderlycare/static/js/14.aa489b4d.chunk.js"
+  },
+  {
+    "revision": "b1da6f0227d7488307e6",
+    "url": "/elderlycare/static/js/15.1fb07dbf.chunk.js"
+  },
+  {
+    "revision": "d840c5151add07f4050b",
+    "url": "/elderlycare/static/js/16.8eaa2207.chunk.js"
+  },
+  {
+    "revision": "d1d8790837a1aff7f83e",
+    "url": "/elderlycare/static/js/17.8a4f0321.chunk.js"
+  },
+  {
+    "revision": "606906d46d16551d14ff",
+    "url": "/elderlycare/static/js/18.b6046cd4.chunk.js"
+  },
+  {
+    "revision": "2791f9409db3a6b6117c",
+    "url": "/elderlycare/static/js/19.73b2bdaa.chunk.js"
+  },
+  {
+    "revision": "0e003f6a35303512009f",
+    "url": "/elderlycare/static/js/2.5b03bc5b.chunk.js"
+  },
+  {
+    "revision": "e9e90cb167d45a3b9406",
+    "url": "/elderlycare/static/js/20.c5f33108.chunk.js"
+  },
+  {
+    "revision": "6de048bc8c1a3acdd537",
+    "url": "/elderlycare/static/js/21.89696c13.chunk.js"
+  },
+  {
+    "revision": "e9946ce45262f4f8abce",
+    "url": "/elderlycare/static/js/22.ed4ae285.chunk.js"
+  },
+  {
+    "revision": "cc95f2aa9bbaa1d4c05b",
+    "url": "/elderlycare/static/js/23.aed51420.chunk.js"
+  },
+  {
+    "revision": "68f6e8e6b5bf76aea0de",
+    "url": "/elderlycare/static/js/24.2ca1ac01.chunk.js"
+  },
+  {
+    "revision": "20f3d5d6de2ac5826b96",
+    "url": "/elderlycare/static/js/25.7afc8ab3.chunk.js"
+  },
+  {
+    "revision": "8b0cefc4e1f24b759e0b",
+    "url": "/elderlycare/static/js/26.3b96aca7.chunk.js"
+  },
+  {
+    "revision": "5bbfeb9409c08a4687d6",
+    "url": "/elderlycare/static/js/27.b69d966e.chunk.js"
+  },
+  {
+    "revision": "3c9752ef4ed74846412f",
+    "url": "/elderlycare/static/js/28.1c661d18.chunk.js"
+  },
+  {
+    "revision": "ebe7fbe56c653b4246c2",
+    "url": "/elderlycare/static/js/29.1c96b0d9.chunk.js"
+  },
+  {
+    "revision": "4a55c1ab0a8dd75df2c7",
+    "url": "/elderlycare/static/js/3.d69ff033.chunk.js"
+  },
+  {
+    "revision": "1bad94e6360cacba01c6",
+    "url": "/elderlycare/static/js/30.590aeadc.chunk.js"
+  },
+  {
+    "revision": "8523f1e34e5a0f24ba42",
+    "url": "/elderlycare/static/js/31.4dd9e44b.chunk.js"
+  },
+  {
+    "revision": "ae642a98839cfbb3255f",
+    "url": "/elderlycare/static/js/32.3372f6d9.chunk.js"
+  },
+  {
+    "revision": "317f9ed9a4e1951d4e9a",
+    "url": "/elderlycare/static/js/33.a825d326.chunk.js"
+  },
+  {
+    "revision": "b042d11f7d2a8ea28a74",
+    "url": "/elderlycare/static/js/34.3ef93249.chunk.js"
+  },
+  {
+    "revision": "ae2525ca2953b1cddd72",
+    "url": "/elderlycare/static/js/35.8c827d4a.chunk.js"
+  },
+  {
+    "revision": "6e4801d64cd5677e977179501d49a9ce",
+    "url": "/elderlycare/static/js/35.8c827d4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb9e769d6050151aa4ef",
+    "url": "/elderlycare/static/js/36.a76f8989.chunk.js"
+  },
+  {
+    "revision": "cf8f4da3379a20401fa7",
+    "url": "/elderlycare/static/js/37.d3a07c19.chunk.js"
+  },
+  {
+    "revision": "9f8982ec282e994cd3d0",
+    "url": "/elderlycare/static/js/38.e56f9a14.chunk.js"
+  },
+  {
+    "revision": "5070b360f8a80466031b",
+    "url": "/elderlycare/static/js/39.0bcf18f4.chunk.js"
+  },
+  {
+    "revision": "9368d03691d43855657a",
+    "url": "/elderlycare/static/js/4.3e98514a.chunk.js"
+  },
+  {
+    "revision": "c360eff85ca112d098c080b19dff9b78",
+    "url": "/elderlycare/static/js/4.3e98514a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbfd4c093e003c5c3884",
+    "url": "/elderlycare/static/js/40.72b35fa0.chunk.js"
+  },
+  {
+    "revision": "c392cb7ab6185e1e9a04",
+    "url": "/elderlycare/static/js/41.bbab175d.chunk.js"
+  },
+  {
+    "revision": "93a12709cc2a84926d5d",
+    "url": "/elderlycare/static/js/42.cc0fe9da.chunk.js"
+  },
+  {
+    "revision": "1166580843ebe36525bf",
+    "url": "/elderlycare/static/js/43.7ee56482.chunk.js"
+  },
+  {
+    "revision": "0360f4ea4f68c86066bd",
+    "url": "/elderlycare/static/js/44.2bc39f0e.chunk.js"
+  },
+  {
+    "revision": "26331a0484302d9f6d5f",
+    "url": "/elderlycare/static/js/45.f8c20445.chunk.js"
+  },
+  {
+    "revision": "68e4ae14e19fcc44d8a0",
+    "url": "/elderlycare/static/js/46.69bd907c.chunk.js"
+  },
+  {
+    "revision": "227b80c3bd3dcf4ded18",
+    "url": "/elderlycare/static/js/47.cd4ce855.chunk.js"
+  },
+  {
+    "revision": "00cbeef20c5703451caa",
+    "url": "/elderlycare/static/js/48.b666ec87.chunk.js"
+  },
+  {
+    "revision": "afa2f24b746eb60dd74e",
+    "url": "/elderlycare/static/js/49.65b7d371.chunk.js"
+  },
+  {
+    "revision": "b6dc2bd10cf53d4f4a2c",
+    "url": "/elderlycare/static/js/5.c49177e3.chunk.js"
+  },
+  {
+    "revision": "dad0a1bf233bb46deb2c",
+    "url": "/elderlycare/static/js/50.0b70c6a2.chunk.js"
+  },
+  {
+    "revision": "39cc939714688f5f739f",
+    "url": "/elderlycare/static/js/51.dc9a481d.chunk.js"
+  },
+  {
+    "revision": "e30c3c63d1eed8252c87",
+    "url": "/elderlycare/static/js/52.c722e493.chunk.js"
+  },
+  {
+    "revision": "d51c64e67d5826e9a585",
+    "url": "/elderlycare/static/js/53.274e3627.chunk.js"
+  },
+  {
+    "revision": "56b27a82046f1ec68d4e",
+    "url": "/elderlycare/static/js/54.b6f6d0cd.chunk.js"
+  },
+  {
+    "revision": "afc7255c2b795bbdb2f4",
+    "url": "/elderlycare/static/js/55.bfccda6a.chunk.js"
+  },
+  {
+    "revision": "465f60a8f5fbe837b4c3",
+    "url": "/elderlycare/static/js/56.ba63614b.chunk.js"
+  },
+  {
+    "revision": "6ef31182b188ccbf551a",
+    "url": "/elderlycare/static/js/57.0eb88d2e.chunk.js"
+  },
+  {
+    "revision": "c28f487af93636e27b3e",
+    "url": "/elderlycare/static/js/6.f29a693c.chunk.js"
+  },
+  {
+    "revision": "acb7f4a9ed9965622c98",
+    "url": "/elderlycare/static/js/60.2f196a39.chunk.js"
+  },
+  {
+    "revision": "f6a33b08b8dab27dd8ab525dc9994832",
+    "url": "/elderlycare/static/js/60.2f196a39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39311c8aae340a13d8b2",
+    "url": "/elderlycare/static/js/61.3346ce4f.chunk.js"
+  },
+  {
+    "revision": "9a73b17d3f74e8a595e1554cd96c873a",
+    "url": "/elderlycare/static/js/61.3346ce4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55aff61b3dabd0769c72",
+    "url": "/elderlycare/static/js/62.b497914a.chunk.js"
+  },
+  {
+    "revision": "18d09a5eb71beca5a8e8",
+    "url": "/elderlycare/static/js/7.6d0c29a4.chunk.js"
+  },
+  {
+    "revision": "2b1eaa56d239831a2f48",
+    "url": "/elderlycare/static/js/8.653cee81.chunk.js"
+  },
+  {
+    "revision": "dc7f66df94000bba431a",
+    "url": "/elderlycare/static/js/9.3eb3a831.chunk.js"
+  },
+  {
+    "revision": "ab455a83a6c46b0f6035",
+    "url": "/elderlycare/static/js/main.d5c72da6.chunk.js"
+  },
+  {
+    "revision": "44f25d254205032bc06b",
+    "url": "/elderlycare/static/js/runtime-main.6b3d37c5.js"
+  },
+  {
+    "revision": "74682ec33acb1a883ae7d81e961610ce",
+    "url": "/elderlycare/static/media/BikoBold.74682ec3.woff"
+  },
+  {
+    "revision": "8dc1f108ff47b9b3a43b00d967d98c12",
+    "url": "/elderlycare/static/media/BikoBold.8dc1f108.woff2"
+  },
+  {
+    "revision": "6975fe7d57dfcc2f3e8363645e37159e",
+    "url": "/elderlycare/static/media/SFProDisplay.6975fe7d.woff2"
+  },
+  {
+    "revision": "8fa494e339ff1ff5b7f9c9671b1a26ca",
+    "url": "/elderlycare/static/media/SFProDisplay.8fa494e3.woff"
+  },
+  {
+    "revision": "026bd4c6e82238a6ffadb7ab27164105",
+    "url": "/elderlycare/static/media/about-us.026bd4c6.svg"
+  },
+  {
+    "revision": "f37973b858f096b18620828dce7e833f",
+    "url": "/elderlycare/static/media/arrowLeft.f37973b8.svg"
+  },
+  {
+    "revision": "ef80cf23eca970abbc43c30e0aa3245b",
+    "url": "/elderlycare/static/media/arrowRight.ef80cf23.svg"
+  },
+  {
+    "revision": "b8150046a278c5b940e1e4f7bcdce79b",
+    "url": "/elderlycare/static/media/arrowUp.b8150046.svg"
+  },
+  {
+    "revision": "9deb1190240f30a1caa1fec4e9110522",
+    "url": "/elderlycare/static/media/aws-logo.9deb1190.png"
+  },
+  {
+    "revision": "d1a51c27631828ea782744f98e66cf4d",
+    "url": "/elderlycare/static/media/breath-right.d1a51c27.svg"
+  },
+  {
+    "revision": "02b562fab59632699c0c6c7d85550a6d",
+    "url": "/elderlycare/static/media/checkCircle.02b562fa.svg"
+  },
+  {
+    "revision": "06c4d4f14f19ff0f59bc253da57dbea7",
+    "url": "/elderlycare/static/media/chevronRight.06c4d4f1.svg"
+  },
+  {
+    "revision": "cc5de8c496e8e47ed7180728b257b752",
+    "url": "/elderlycare/static/media/clouds.cc5de8c4.svg"
+  },
+  {
+    "revision": "87fa5bc869d3a6938243d2db61706e63",
+    "url": "/elderlycare/static/media/cough-right.87fa5bc8.svg"
+  },
+  {
+    "revision": "13e841fb48d6ad8b36b9042eceb2ee6c",
+    "url": "/elderlycare/static/media/cross.13e841fb.svg"
+  },
+  {
+    "revision": "2c8fa176eda3cbcf55cdf98ae18fa028",
+    "url": "/elderlycare/static/media/crossPurple.2c8fa176.svg"
+  },
+  {
+    "revision": "e5a14e374b944cf2d720931b8e646e32",
+    "url": "/elderlycare/static/media/curved-arrow.e5a14e37.svg"
+  },
+  {
+    "revision": "a204147b580465ee02fa489f9ebdb2c6",
+    "url": "/elderlycare/static/media/download.a204147b.svg"
+  },
+  {
+    "revision": "2f56fd773b752c378ce6760ece3ff5ea",
+    "url": "/elderlycare/static/media/exclamationCircle.2f56fd77.svg"
+  },
+  {
+    "revision": "d55011c894c2edcc40658798f7925307",
+    "url": "/elderlycare/static/media/pause.d55011c8.svg"
+  },
+  {
+    "revision": "6f092911a8cbe69fa6ee5d3bc38af57f",
+    "url": "/elderlycare/static/media/play.6f092911.svg"
+  },
+  {
+    "revision": "1fa7aab3b4955b0268697897870894c7",
+    "url": "/elderlycare/static/media/processing.1fa7aab3.svg"
+  },
+  {
+    "revision": "e8e18e64122ad16927f43f63f55005fc",
+    "url": "/elderlycare/static/media/social-distancing.e8e18e64.svg"
+  },
+  {
+    "revision": "1023f9274dcfe54e3e827bd1d7fb104d",
+    "url": "/elderlycare/static/media/start.1023f927.svg"
+  },
+  {
+    "revision": "c2f5fdba24d15a6a7cbf04f4fc447e10",
+    "url": "/elderlycare/static/media/stop.c2f5fdba.svg"
+  },
+  {
+    "revision": "e35eb772124d1620bd44fec74b124293",
+    "url": "/elderlycare/static/media/upload.e35eb772.svg"
+  },
+  {
+    "revision": "f709f19d816590c2b321c831b78c1bf5",
+    "url": "/elderlycare/static/media/virufyLogo3x.f709f19d.png"
+  },
+  {
+    "revision": "d5694ca4d8b884d64a1b5ffda9083622",
+    "url": "/elderlycare/static/media/virufyLogoWhiteBox.d5694ca4.svg"
+  },
+  {
+    "revision": "ab396c8239c027c7bbe37ba622d9ce9c",
+    "url": "/elderlycare/static/media/voice-right.ab396c82.svg"
+  },
+  {
+    "revision": "76da4893218242af0f9384d418e7a5f0",
+    "url": "/elderlycare/static/media/womanWithPhoneSide.76da4893.svg"
+  }
+]);
