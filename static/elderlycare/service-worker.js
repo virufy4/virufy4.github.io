ed113f5b477d2014121047a8a5fcef18550cffa2
elsy0111 <kpp0193@gmail.com>
2025-03-21 11:17:42 +0900

    fixed routing and diagnosis (reupload)

--- static/elderlycare/service-worker.js
+++ static/elderlycare/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/elderlycare/precache-manifest.38b160422e88613eae0c0b79199369ce.js"
+  "/precache-manifest.64450ee1249ae884410160e296c820de.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/elderlycare/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
